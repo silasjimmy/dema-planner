81d4170d5e4d60a4eadf17d6b824cc415f9b2896
Silas Jimmy <jimmysilas17@gmail.com>
2022-01-19 20:57:02 +0300

    refactor: add toolbar to home

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-app-bar app elevate-on-scroll color="white">
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title> Dema </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn plain link class="text-none" to="/home">Home</v-btn>
+      <v-btn plain link class="text-none" to="/sign-in">Log in</v-btn>
+      <v-btn plain link class="text-none" to="/sign-up">Sign up</v-btn>
+    </v-app-bar>
+
     <v-main class="b">
       <v-container fluid>
         <router-view />
