da233775f241ea7eaa01a070e443561843944bd1
Silas Jimmy <jimmysilas17@gmail.com>
2022-01-19 20:37:28 +0300

    add: create log in view

---
+++ src/components/SignIn.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row class="text-center b">
+    <v-col cols="4" class="mx-auto">
+      <h1 class="text-h4 my-4">Welcome back!</h1>
+      <v-text-field
+        dense
+        outlined
+        clearable
+        rounded
+        color="success"
+        prepend-icon="mdi-email"
+        v-model="email"
+        label="Email address"
+        type="email"
+      ></v-text-field>
+      <v-text-field
+        dense
+        outlined
+        rounded
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="() => (showPassword = !showPassword)"
+        color="success"
+        prepend-icon="mdi-lock"
+        v-model="password"
+        label="Password"
+        type="password"
+      ></v-text-field>
+      <v-btn rounded @click="login" color="success" class="text-none"
+        >Log in</v-btn
+      >
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      email: "",
+      password: "",
+      showPassword: false,
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password);
+
+      //   const auth = getAuth();
+      //   signInWithEmailAndPassword(auth, this.email, this.password)
+      //     .then((userCredential) => {
+      //       console.log(userCredential);
+
+      //       // Redirect to dashboard
+      //       this.$router.push({ path: "easter-egg" });
+      //     })
+      //     .catch((error) => {
+      //       console.log(error.message);
+      //     });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,24 @@ import VueRouter from 'vue-router'
 
 import EasterEgg from '@/components/EasterEgg.vue';
 import Home from '@/components/Home.vue';
+import SignIn from '@/components/SignIn.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/sign-in',
+    name: 'sign-in',
+    component: SignIn
   },
   {
     path: '/easter-egg',
