47784eebe5a3535d203668e5a3ab358c5c844e69
Silas Jimmy <jimmysilas17@gmail.com>
2022-01-22 16:17:38 +0300

    refactor: add user authentication details in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import SET_USER_EMAIL from './mutation-types'
+import SET_USER_ROLE from './mutation-types'
+import SET_AUTHENTICATION from './mutation-types'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userEmail: '',
+    authenticated: false,
+    userRole: ''
   },
   mutations: {
+    [SET_USER_EMAIL](state, email) {
+      state.userEmail = email
+    },
+    [SET_USER_ROLE](state, role) {
+      state.userRole = role
+    },
+    [SET_AUTHENTICATION](state, status) {
+      state.authenticated = status
+    },
   },
   actions: {
   },
---
+++ src/store/mutation-types.js
@@ -0,0 +1,3 @@
+export const SET_USER_EMAIL = 'setUserEmail'
+export const SET_AUTHENTICATION = 'setAuthentication';
+export const SET_USER_ROLE = 'setUserRole';
