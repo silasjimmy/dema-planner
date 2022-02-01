be6880c78573e95fcf6682e8f050f9cf490c565d
Silas Jimmy <jimmysilas17@gmail.com>
2022-02-01 08:07:48 +0300

    refactor: create eatery details page layout

--- src/App.vue
+++ src/App.vue
@@ -379,52 +379,56 @@
 </template>
 
 <script>
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  getAuth,
+  // onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 import { mapState } from "vuex";
 import MealsInfo from "./components/MealsInfo.vue";
 
 export default {
   name: "App",
   created() {
-    // Sets the sign in status, user email and user role in store on app creation
-    this.$store.commit(
-      "setSignedIn",
-      localStorage.getItem("loggedIn") === "true"
-    );
-    this.$store.commit("setUserEmail", localStorage.getItem("userEmail"));
-    this.$store.commit("setUserRole", localStorage.getItem("userRole"));
+    // // Sets the sign in status, user email and user role in store on app creation
+    // this.$store.commit(
+    //   "setSignedIn",
+    //   localStorage.getItem("loggedIn") === "true"
+    // );
+    // this.$store.commit("setUserEmail", localStorage.getItem("userEmail"));
+    // this.$store.commit("setUserRole", localStorage.getItem("userRole"));
 
-    // localStorage.setItem("loggedIn", "true");
-    // localStorage.setItem("userRole", "consumer");
-    // this.$store.commit("setSignedIn", true);
-    // this.$store.commit("setUserRole", "consumer");
+    localStorage.setItem("loggedIn", "true");
+    localStorage.setItem("userRole", "consumer");
+    this.$store.commit("setSignedIn", true);
+    this.$store.commit("setUserRole", "consumer");
   },
-  mounted() {
-    // Monitor the user sign in activity
-    const auth = getAuth();
+  // mounted() {
+  //   // Monitor the user sign in activity
+  //   const auth = getAuth();
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // Store the user email locally
-        localStorage.setItem("userEmail", user.email);
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // Store the user email locally
+  //       localStorage.setItem("userEmail", user.email);
 
-        // Set logged in to true
-        localStorage.setItem("loggedIn", "true");
-      } else {
-        // Remove user email from local storage
-        localStorage.removeItem("userEmail");
+  //       // Set logged in to true
+  //       localStorage.setItem("loggedIn", "true");
+  //     } else {
+  //       // Remove user email from local storage
+  //       localStorage.removeItem("userEmail");
 
-        // Remove user role from local storage
-        localStorage.removeItem("userRole");
+  //       // Remove user role from local storage
+  //       localStorage.removeItem("userRole");
 
-        // Set logged in to false
-        localStorage.setItem("loggedIn", "false");
+  //       // Set logged in to false
+  //       localStorage.setItem("loggedIn", "false");
 
-        // Update app store
-        this.$store.commit("setSignedIn", false);
-      }
-    });
-  },
+  //       // Update app store
+  //       this.$store.commit("setSignedIn", false);
+  //     }
+  //   });
+  // },
   data() {
     return {
       homeSidenav: false,
--- src/components/EateryDetails.vue
+++ src/components/EateryDetails.vue
@@ -1,8 +1,85 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h1>Eatery details</h1>
+  <v-container fluid class="b">
+    <v-row class="b">
+      <v-btn icon>
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </v-row>
+    <v-row class="b">
+      <v-col cols="5">
+        <v-img class="b" height="200px"></v-img>
+      </v-col>
+      <v-col cols="7" align-self="end">
+        <div class="b">
+          <h1>Eatery name</h1>
+          <h2>Eatery bio</h2>
+          <h3>Eatery rating</h3>
+        </div>
+      </v-col>
+      <v-col cols="5">
+        <v-list two-line class="py-0">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon> mdi-map-marker </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Address</v-list-item-title>
+              <v-list-item-subtitle>City, Country</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon> mdi-email </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>name@domain.com</v-list-item-title>
+              <v-list-item-subtitle>Email address</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon> mdi-phone </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>+0 000 000 000</v-list-item-title>
+              <v-list-item-subtitle>Phone number</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>www.domainname.com</v-list-item-title>
+              <v-list-item-subtitle>Website</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item>
+            <v-btn block>
+              <v-icon left>mdi-message-text</v-icon>
+              Send a message
+            </v-btn>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-col cols="7">
+        <div class="b">
+          <h1>Menu</h1>
+        </div>
+        <div class="b text-center">
+          <p>You have meals today in this restaurant</p>
+          <v-btn>Reserve a seat</v-btn>
+        </div>
       </v-col>
     </v-row>
   </v-container>
