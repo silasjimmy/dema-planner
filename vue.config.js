87b3042789a6a6b3b46426b89ee3cf2735085e97
Silas Jimmy <jimmysilas17@gmail.com>
2022-03-02 13:25:37 +0300

    refactor: remove workbox plugin mode

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,12 @@ module.exports = {
     // appleMobileWebAppCapable: 'yes',
     // appleMobileWebAppStatusBarStyle: 'black',
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
   }
 }
