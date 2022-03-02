3e35630ebc549ed311df02ba580e88aef5657bce
Silas Jimmy <jimmysilas17@gmail.com>
2022-03-02 13:08:35 +0300

    refactor: add pwa configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    name: 'Dema',
+    // themeColor: '#4DBA87',
+    // msTileColor: '#000000',
+    // appleMobileWebAppCapable: 'yes',
+    // appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 }
