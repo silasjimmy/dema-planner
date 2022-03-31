633af22e0d1166d3bf37f9d17b693c52d92f0ea1
Silas Jimmy <jimmysilas17@gmail.com>
2022-03-31 05:30:19 +0300

    refactor: add pwa configuration

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,21 @@ module.exports = {
       enableInSFC: true,
       enableBridge: false
     }
+  },
+
+  pwa: {
+    name: 'Dema',
+    themeColor: '#4CAF50',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
   }
 }
